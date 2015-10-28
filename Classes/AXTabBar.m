305936d8d7f0ead40faf3926eaa668b97aadbfaf
Kelly Bennett <kelly.bennett@leafly.com>
2015-10-27 18:05:24 -0700

    Forgot to save this file in my last commit

--- Classes/AXTabBar.m
+++ Classes/AXTabBar.m
@@ -9,7 +9,7 @@
 @interface AXTabBar ()
 @property (readonly, nonatomic) UIScrollView *containerView;
 @property (readonly, nonatomic) UIToolbar *toolbar;
-@property (copy, nonatomic) NSArray *tabBarItemButtons;
+
 @end
 
 @implementation AXTabBar {
